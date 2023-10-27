44e7f898ab76facc3d5302a56c71cdfe14372c05
Stuart Blackburn <blackburns@vmware.com>
2023-10-27 17:01:13 -0400

    dev

--- func.py
+++ func.py
@@ -5,6 +5,6 @@ from app import app  # Import the Flask app
 def main(req):
     # Main application logic goes here
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    if __name__ == '__main__':
+        app.run(debug=True)
 
