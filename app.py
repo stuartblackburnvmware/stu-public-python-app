968881f68b2deb29d34fc974a3e7e00005f9dfe0
Stuart Blackburn <blackburns@vmware.com>
2023-10-27 16:57:16 -0400

    dev

--- app.py
+++ app.py
@@ -12,11 +12,13 @@ def hello_world():
 @app.route('/api/items', methods=['GET'])
 def get_items():
     # Implement logic to retrieve and return a list of items
+    return "List of items"
 
 # API to get details of a specific item
 @app.route('/api/items/<int:item_id>', methods=['GET'])
 def get_item(item_id):
     # Implement logic to retrieve and return details of the specified item
+    return f"Details of item {item_id}"
 
 if __name__ == '__main__':
     app.run(debug=True)
