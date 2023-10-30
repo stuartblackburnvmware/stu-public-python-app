6756e96338cbe7ae23251ed5f7f180bf3b8034b3
Stuart Blackburn <blackburns@vmware.com>
2023-10-30 12:46:15 -0400

    dev

--- func.py
+++ func.py
@@ -1,18 +1,5 @@
-from flask import Flask, jsonify
+from typing import Any
 
-app = Flask(__name__)
-
-@app.route('/')
-def main():
-    return "This is the greatest PUBLIC Python app ever written. Trust me."
-
-@app.route('/api2')
-def url2():
-    return "This is the 2nd api endpoint"
-
-@app.route('/api3')
-def url3():
-    return "This is the 3rd api endpoint"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+def main(req: Any):
+    # Your function implementation goes here
+    return "This is the greatest PUBLIC python app ever written. Trust me."
