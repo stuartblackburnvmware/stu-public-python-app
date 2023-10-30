494f9ac03e59d57ec306f03e698af74656e65da9
Stuart Blackburn <blackburns@vmware.com>
2023-10-30 13:08:34 -0400

    dev

--- test_func.py
+++ test_func.py
@@ -1,17 +1,12 @@
-# test_func.py
-
 import unittest
-from func import app  # Import the Flask app
+from func import main  # Import the main function to test
 
 class TestFunc(unittest.TestCase):
     def test_main_returns_expected_output(self):
         # Test the main function
-        with app.test_client() as client:
-            response = client.get('/')
-            data = response.data.decode('utf-8')
-            expected_output = "This is the greatest PUBLIC python app ever written. Trust me."
-            self.assertEqual(data, expected_output)
+        result = main(None)  # You can pass None for the 'req' argument
+        expected_output = "This is the greatest PUBLIC python app ever written. Trust me."
+        self.assertEqual(result, expected_output)
 
 if __name__ == '__main__':
     unittest.main()
-
