3095eb673a25cb1ca0e1b9ba4a36bc98a3b70943
Stuart Blackburn <blackburns@vmware.com>
2023-11-09 13:19:15 -0500

    dev

--- func.py
+++ func.py
@@ -2,4 +2,4 @@ from typing import Any
 
 def main(req: Any):
     # Your function implementation goes here
-    return "This is the greatest PUBLIC python app ever written. Trust me."
+    return "This is the greatest PUBLIC2 python app ever written. Trust me."
